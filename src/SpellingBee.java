ab6287a9e2fecf5662a62fea57b81c241e52f2d4
iDroid-User <steventejada6@gmail.com>
2026-04-19 18:39:44 -0700

    Make `puzzle` and `solutions` member variables
    Puzzle validation and dictionary transcription are cached. They are calculated once and assigned to member variables.

--- src/SpellingBee.java
+++ src/SpellingBee.java
@@ -16,41 +16,63 @@ public class SpellingBee {
 
     public void run() {
         sbg = new SpellingBeeGraphics();
-
+        totalScore = 0;
         // Add two interactors to the control strip at the bottom of the window
         // The calls to addField() and addButton() specify the actions that occur when the user triggers events
         sbg.addField("Puzzle", (s) -> puzzleAction(s)); // Calls puzzleAction(), passing the puzzle string
         sbg.addButton("Solve", (s) -> solveAction()); // Calls solveAction()
+        sbg.addField("Word", (s) -> wordAction(s), 7);
     }
 
     private void puzzleAction(String s) {
         String error = validatePuzzle(s);
-        if (error == null)
+        if (error == null) {
             sbg.setBeehiveLetters(s); // Display puzzle letters on the screen
-        else
+            puzzle = sbg.getBeehiveLetters().trim(); // Fetch data at the top level, then pass it down to helpers!
+        } else
             sbg.showMessage(error, Color.RED); // Tells the user WHY the puzzle failed
     }
 
     private void solveAction() {
-        String puzzle = sbg.getBeehiveLetters().trim(); // Fetch data at the top level, then pass it down to helpers!
         // getBeehiveLetters() returns default value (spaces) if Solve is clicked before a valid puzzle is set
         if (puzzle.isEmpty()) {
             sbg.showMessage("Please enter a puzzle first.", Color.RED);
             return;
         }
-        ArrayList<String> solutions = new ArrayList<>(validateWords(puzzle));
-        int totalScore = 0;
+        solutions = new ArrayList<>(validateWords(puzzle));
         for (String solution : solutions) {
             int score = scoreWord(solution, puzzle); totalScore += score;
             Color color = isPangram(solution, puzzle) ? Color.BLUE : Color.BLACK;
             sbg.addWord(solution + " (" + score + ")", color);
         }
         sbg.showMessage(solutions.size() + " words; " + totalScore + " points", Color.BLACK);
+        // Reset score after solving
+//        sbg.clearWordList();
+        totalScore = 0;
+    }
+
+    private void wordAction(String s) {
+        String attempt = sbg.getField("Word");
+
+        if (puzzle.isEmpty()) {
+            sbg.showMessage("Please enter a puzzle first.", Color.RED);
+            return;
+        }
+        // Check legality before traversing the list of words to validate
+        if (isWordLegal(attempt, puzzle)) {
+            if (solutions.contains(attempt)) {
+                int score = scoreWord(attempt, puzzle); totalScore += score;
+                Color color = isPangram(attempt, puzzle) ? Color.BLUE : Color.BLACK;
+                sbg.addWord(attempt + " (" + score + ")", color);
+                solutions.remove(attempt);
+                sbg.showMessage(solutions.size() + " words; " + totalScore + " points", Color.BLACK);
+                sbg.setField("Word", ""); // UX feature: clear field after an acceptable word is entered
+            }
+        }
     }
 
     // Decompose the puzzleAction problem: check whether a puzzle is legal
     private String validatePuzzle(String s) {
-//        s = s.toUpperCase(); // Uniform case
         if (s.length() == 7) {
             for (int i = 0; i < s.length(); i++) {
                 char c = s.charAt(i);
@@ -96,26 +118,26 @@ public class SpellingBee {
     }
 
     // Decompose the solveAction problem: read the dictionary and check each word
-    private ArrayList<String> validateWords(String p) {
+    private ArrayList<String> validateWords(String puzz) {
         ArrayList<String> words = new ArrayList<>(readDictionary()); // Assign a shallow copy of the return ArrayList
         ArrayList<String> legalWords = new ArrayList<>();
 
         for (String word : words) {
-            if (isWordLegal(word, p)) legalWords.add(word);
+            if (isWordLegal(word, puzz)) legalWords.add(word);
         }
         return legalWords;
     }
 
     // Decompose the solveAction problem: display scores following the words that they credit
-    private int scoreWord(String w, String puzz) {
+    private int scoreWord(String word, String puzz) {
         int score = 1;
-        if (w.length() > 4) {
-            score *= w.length(); // Words 4 letters or longer score a point per letter
+        if (word.length() > 4) {
+            score *= word.length(); // Words 4 letters or longer score a point per letter
             // A 7-letter word is likely a pangram
-            if (w.length() >= 7) {
+            if (word.length() >= 7) {
                 for (int i = 0; i < puzz.length(); i++) {
                     // Make character comparison case-insensitive
-                    if (w.indexOf(Character.toLowerCase(puzz.charAt(i))) == -1) return score; // Not a pangram
+                    if (word.indexOf(Character.toLowerCase(puzz.charAt(i))) == -1) return score; // Not a pangram
                 }
                 return score + 7; // 7-point pangram bonus applied
             }
@@ -124,13 +146,18 @@ public class SpellingBee {
     }
 
     // Decompose the solveAction problem: color words accordingly
-    private boolean isPangram(String w, String puzz) {
-        if (w.length() >= 7) {
+    private boolean isPangram(String word, String puzz) {
+        if (word.length() >= 7) {
             for (int i = 0; i < puzz.length(); i++) {
-                if (w.indexOf(Character.toLowerCase(puzz.charAt(i))) == -1) return false; // Not a pangram
+                if (word.indexOf(Character.toLowerCase(puzz.charAt(i))) == -1) return false; // Not a pangram
             }
             return true; // A pangram
         } else return false; // Word does not contain all puzzle letters
+    }
+
+    // Decompose the wordAction problem: find the word in the puzzle
+    private String findWord(String w, ArrayList<String> l) {
+        return "";
     }
 
 /* Constants */
@@ -140,6 +167,9 @@ public class SpellingBee {
 /* Private instance variables */
 
     private SpellingBeeGraphics sbg;
+    private String puzzle;
+    private ArrayList<String> solutions;
+    private int totalScore;
 
 /* Startup code */
 
