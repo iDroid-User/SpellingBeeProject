3640d7c78cdc2d395fbde22a873ea882d14b5e99
iDroid-User <steventejada6@gmail.com>
2026-04-20 19:09:19 -0700

    Refactor `puzzleAction()`
    A call to the function implies a new puzzle, so `sbg.clearWordList()` clears the word list for the previous puzzle. The total score is now reset as part of the implicit puzzle definition. `solutions` are also queried in tandem.

--- src/SpellingBee.java
+++ src/SpellingBee.java
@@ -16,7 +16,6 @@ public class SpellingBee {
 
     public void run() {
         sbg = new SpellingBeeGraphics();
-        totalScore = 0;
         // Add two interactors to the control strip at the bottom of the window
         // The calls to addField() and addButton() specify the actions that occur when the user triggers events
         sbg.addField("Puzzle", (s) -> puzzleAction(s)); // Calls puzzleAction(), passing the puzzle string
@@ -25,30 +24,31 @@ public class SpellingBee {
     }
 
     private void puzzleAction(String s) {
+        sbg.clearWordList();
+        totalScore = 0; // New puzzle, new score
         String error = validatePuzzle(s);
         if (error == null) {
             sbg.setBeehiveLetters(s); // Display puzzle letters on the screen
+            // getBeehiveLetters() returns default value (spaces) if Solve is clicked before a valid puzzle is set
             puzzle = sbg.getBeehiveLetters().trim(); // Fetch data at the top level, then pass it down to helpers!
+            solutions = new ArrayList<>(validateWords(puzzle));
         } else
             sbg.showMessage(error, Color.RED); // Tells the user WHY the puzzle failed
     }
 
     private void solveAction() {
-        // getBeehiveLetters() returns default value (spaces) if Solve is clicked before a valid puzzle is set
+        sbg.clearWordList(); // Resets the previous window
         if (puzzle.isEmpty()) {
             sbg.showMessage("Please enter a puzzle first.", Color.RED);
             return;
         }
-        solutions = new ArrayList<>(validateWords(puzzle));
         for (String solution : solutions) {
             int score = scoreWord(solution, puzzle); totalScore += score;
             Color color = isPangram(solution, puzzle) ? Color.BLUE : Color.BLACK;
             sbg.addWord(solution + " (" + score + ")", color);
         }
         sbg.showMessage(solutions.size() + " words; " + totalScore + " points", Color.BLACK);
-        // Reset score after solving
-//        sbg.clearWordList();
-        totalScore = 0;
+        totalScore = 0; // Keeps score constant after a refresh
     }
 
     private void wordAction(String s) {
